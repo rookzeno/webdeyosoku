f409b8acd6aff296688ce05778b8581b8fded5fc
oo oo <yusha30over@yahoo.co.jp>
2018-12-13 20:56:51 +0900

    add new file

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, abort, make_response,render_template,url_for,request,redirect
 import os.path
+from imagenet import imagenet
 
 app = Flask(__name__)
 
@@ -14,9 +15,14 @@ def posttest():
     img_file = request.files['img_file']
     fileName = img_file.filename
     root, ext = os.path.splitext(fileName)
-    gazouketori = set()
+    ext = ext.lower()
+    gazouketori = set([".jpg", ".jpeg", ".jpe", ".jp2", ".png", ".webp", ".bmp", ".pbm", ".pgm", ".ppm",
+                      ".pxm", ".pnm",  ".sr",  ".ras", ".tiff", ".tif", ".exr", ".hdr", ".pic", ".dib"])
     if ext not in gazouketori:
         return render_template('index.html',massege = "OpenCVが対応してない拡張子です",color = "red")
+    print("success")
+    try:
+        bunrui = imagenet(img_file)
     return render_template('index.html')
 
 
@@ -26,4 +32,4 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
-    api.run()
+    app.run()
---
+++ imagenet.py
@@ -0,0 +1,4 @@
+import numpy as np
+class imagenet():
+    def __init__(self):
+        pass
