7fd9788d35c811c0749f946e0b7a90d585e320e6
rookzeno <wawawa@example.com>
2018-12-14 00:13:12 +0900

    画像

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, jsonify, abort, make_response,render_template,url_for,request,redirect
+from flask import Flask, jsonify, abort, make_response,render_template,url_for,request,redirect,send_file
 import os.path
 from imagenet import imagenet
+from PIL import Image
+import io
+import qrcode as qr
+import base64
 
 app = Flask(__name__)
 
@@ -19,12 +23,23 @@ def posttest():
     gazouketori = set([".jpg", ".jpeg", ".jpe", ".jp2", ".png", ".webp", ".bmp", ".pbm", ".pgm", ".ppm",
                       ".pxm", ".pnm",  ".sr",  ".ras", ".tiff", ".tif", ".exr", ".hdr", ".pic", ".dib"])
     if ext not in gazouketori:
-        return render_template('index.html',massege = "OpenCVが対応してない拡張子です",color = "red")
+        return render_template('index.html',massege = "対応してない拡張子です",color = "red")
     print("success")
     try:
         bunrui = imagenet(img_file)
-    return render_template('index.html')
+        name, desc, score =  bunrui.deep()
+    except:
+        return render_template('index.html',massege = "解析出来ませんでした",color = "red")
+    buf = io.BytesIO()
+    image = Image.open(img_file)
+    image.save(buf, 'png')
+    qr_b64str = base64.b64encode(buf.getvalue()).decode("utf-8")
+    qr_b64data = "data:image/png;base64,{}".format(qr_b64str)
+    return render_template('kekka.html',namae = desc ,kaku = score,img = qr_b64data)
 
+@app.route('/kekka')
+def kekka():
+    return render_template('kekka.html')
 
 # エラーハンドリング
 @app.errorhandler(404)
--- imagenet.py
+++ imagenet.py
@@ -1,4 +1,27 @@
 import numpy as np
+from keras.preprocessing import image
+from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
+
 class imagenet():
-    def __init__(self):
-        pass
+    def __init__(self,img):
+        img = image.load_img(img, target_size=(224, 224))
+        img = image.img_to_array(img).reshape(1,224,224,3)
+        self.img = img
+    def load_image(filename):
+        img = image.load_img(filename, target_size=(224, 224))
+        return image.img_to_array(img)
+    def deep(self):
+        model = ResNet50(include_top=True, weights='imagenet',
+              input_tensor=None, input_shape=None)
+        pred = model.predict(preprocess_input(self.img))
+        top = decode_predictions(pred, top=5)
+        name = []
+        desc = []
+        score = []
+        for i in range(0, len(top)):
+            for j in range(0, len(top[i])):
+                name1, desc1, score1 = top[i][j]
+                name.append(name1)
+                desc.append(desc1)
+                score.append(score1*100)
+        return name, desc, score
